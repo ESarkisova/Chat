0204b13936d4b812f23e59957bad9f285049c92c
Елена <s_elena-@bk.ru>
2020-04-14 09:57:19 +0400

    Updates

---
+++ precache-manifest.0b816cfbfc2ebf20df12af2acdc8483c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "440ac919f30e014564d9ad3a0d1a722b",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "7eba5ad5923669bd1059",
+    "url": "/chat/static/css/main.e3d87ad5.chunk.css"
+  },
+  {
+    "revision": "e9a0ff5769ea89ad76e5",
+    "url": "/chat/static/js/2.05dee868.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/chat/static/js/2.05dee868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eba5ad5923669bd1059",
+    "url": "/chat/static/js/main.f9313419.chunk.js"
+  },
+  {
+    "revision": "b0b07b77651e8ac97d34",
+    "url": "/chat/static/js/runtime-main.de3f8812.js"
+  }
+]);
